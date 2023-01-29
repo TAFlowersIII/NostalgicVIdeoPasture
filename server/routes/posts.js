c6afbb2336d06bf9a8581f01a54bef31afcb7403
KhakiSuitValentino <110346660+TAFlowersIII@users.noreply.github.com>
2023-01-29 16:17:35 -0500

    test push to confirm pushing is working properly

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js'
 
+//test 
+
 const router = express.Router();
 
 router.get('/', getPosts);
